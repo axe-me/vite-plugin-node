c848c19de60af867373773cd2a9d5aa8a9048916
Axe <shane369@gmail.com>
2021-02-23 17:48:36 -0500

    wip

--- src/vite-node.ts
+++ src/vite-node.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import { exit } from 'process';
 import { createServer, HMRPayload } from 'vite';
 import { w3cwebsocket as WebSocketClient } from 'websocket';
@@ -7,25 +8,32 @@ import { MakeServer } from './servers/server-factory';
 async function bootstrap() {
   // vite server config is managed by the plugin
   const viteServer = await createServer();
+  const logger = viteServer.config.logger;
+  logger.clearScreen('info');
+  logger.info(chalk.yellow`Vite Dev Server Created \n`, { timestamp: true });
 
   const wsc = new WebSocketClient(`ws://localhost:${WS_PORT}`);
-
-  const logger = viteServer.config.logger;
 
   const nodeServer = await MakeServer(viteServer);
 
   wsc.onmessage = async ({ data }) => {
     const payload = JSON.parse(data as string) as HMRPayload;
-    logger.info(data as string);
+    logger.info(data as string, { timestamp: true });
     if (payload.type === 'connected') {
+      logger.clearScreen('info');
+      logger.info(chalk.green`Connected to Vite Dev Server \n`, { timestamp: true });
+
       await nodeServer.start();
+
+      logger.info(chalk.green`Node Server Started \n`, { timestamp: true });
     } else if (payload.type === 'error') {
-      logger.error('something went wrong from HMR');
+      logger.error(chalk.red`Something went wrong with HMR, shutting down... \n`, { timestamp: true });
       await nodeServer.close();
       wsc.close();
       exit(1);
     } else {
       await nodeServer.restart();
+      logger.info(chalk.yellow`Node Server Reloaded \n`, { timestamp: true });
     }
   };
 }
