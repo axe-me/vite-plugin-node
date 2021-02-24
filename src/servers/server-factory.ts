e1bb48026d9160730fa33dfa262bfa0ffbd14e85
Axe <shane369@gmail.com>
2021-02-24 02:54:16 -0500

    fix ws client

--- src/servers/server-factory.ts
+++ src/servers/server-factory.ts
@@ -1,6 +1,8 @@
+import chalk from 'chalk';
 import { exit } from "process";
-import { Plugin, ViteDevServer } from "vite";
-import { IServer, PLUGIN_NAME, SupportedServer, ViteConfig, VitePluginNodeConfig } from "..";
+import { HMRPayload, Plugin, ViteDevServer } from "vite";
+import { client as WebSocketClient } from 'websocket';
+import { IServer, PLUGIN_NAME, SupportedServer, ViteConfig, VitePluginNodeConfig, WS_PORT } from "..";
 import { ExpressServer } from "./express-server";
 import { NestServer } from "./nest-server";
 
@@ -35,11 +37,60 @@ export const MakeServer = async (server: ViteDevServer): Promise<IServer> => {
 
   if (nodeServer) {
     await nodeServer.create(server, config);
+    createWebsoketClient(server, nodeServer, config);
   } else {
-    console.error('Failed to create node server')
+    console.error('Failed to create node server');
     process.exit(1)
   }
 
   return nodeServer;
+}
+
+const createWebsoketClient = (server: ViteDevServer, nodeServer: IServer, config: VitePluginNodeConfig): WebSocketClient => {
+  const wsc = new WebSocketClient();
+  const logger = server.config.logger;
+
+  wsc.on('connectFailed', async (error) => {
+      logger.info(chalk.red`Fail to connect to Vite WebSocket, Error: ${error.toString()}\n`);
+      await nodeServer.close();
+  });
+
+  wsc.on('connect', (connection) => {
+      logger.info('Connected to Vite WebSocket');
+
+      connection.on('error', async (error) => {
+          logger.info(chalk.red`Vite WebSocket Connection Error: ${error.toString()}`);
+          await nodeServer.close();
+      });
+      connection.on('close', async () => {
+          logger.info(chalk.yellow`Vite WebSocket Connection Closed`);
+          await nodeServer.close();
+      });
+      connection.on('message', async (message) => {
+        if (message.type === 'utf8') {
+          const payload = JSON.parse(message.utf8Data as string) as HMRPayload;
+
+          if (payload.type === 'connected') {
+            logger.clearScreen('info');
+            logger.info(chalk.green`Connected to Vite Dev Server \n`, { timestamp: true });
+
+            await nodeServer.start();
+
+            logger.info(chalk.green`Node Server Started \n`, { timestamp: true });
+          } else if (payload.type === 'error') {
+            logger.error(chalk.red`Something went wrong with HMR, shutting down... \n`, { timestamp: true });
+            await nodeServer.close();
+            exit(1);
+          } else { // handle module updates
+            await nodeServer.restart();
+            logger.info(chalk.yellow`Node Server Reloaded \n`, { timestamp: true });
+          }
+        }
+      });
+  });
+
+  wsc.connect(`ws://localhost:${WS_PORT}`);
+
+  return wsc;
 }
 
--- src/vite-node.ts
+++ src/vite-node.ts
@@ -1,8 +1,5 @@
 import chalk from 'chalk';
-import { exit } from 'process';
-import { createServer, HMRPayload } from 'vite';
-import { w3cwebsocket as WebSocketClient } from 'websocket';
-import { WS_PORT } from '.';
+import { createServer } from 'vite';
 import { MakeServer } from './servers/server-factory';
 
 async function bootstrap() {
@@ -12,30 +9,7 @@ async function bootstrap() {
   logger.clearScreen('info');
   logger.info(chalk.yellow`Vite Dev Server Created \n`, { timestamp: true });
 
-  const wsc = new WebSocketClient(`ws://localhost:${WS_PORT}`);
-
-  const nodeServer = await MakeServer(viteServer);
-
-  wsc.onmessage = async ({ data }) => {
-    const payload = JSON.parse(data as string) as HMRPayload;
-    logger.info(data as string, { timestamp: true });
-    if (payload.type === 'connected') {
-      logger.clearScreen('info');
-      logger.info(chalk.green`Connected to Vite Dev Server \n`, { timestamp: true });
-
-      await nodeServer.start();
-
-      logger.info(chalk.green`Node Server Started \n`, { timestamp: true });
-    } else if (payload.type === 'error') {
-      logger.error(chalk.red`Something went wrong with HMR, shutting down... \n`, { timestamp: true });
-      await nodeServer.close();
-      wsc.close();
-      exit(1);
-    } else {
-      await nodeServer.restart();
-      logger.info(chalk.yellow`Node Server Reloaded \n`, { timestamp: true });
-    }
-  };
+  await MakeServer(viteServer);
 }
 
 bootstrap();
