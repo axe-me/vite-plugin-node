79d8f716e6ef516a9af2719fead7c331acfdf184
percy507 <percymong@163.com>
2021-09-19 20:37:18 +0800

    feat: transform `import.meta.url` as nodejs does

--- src/vite-plugin-node.ts
+++ src/vite-plugin-node.ts
@@ -1,4 +1,6 @@
 import { Plugin } from 'vite';
+import path from 'path';
+import { pathToFileURL } from 'url';
 import { PLUGIN_NAME, VitePluginNodeConfig } from '.';
 import { RollupPluginSwc } from "./rollup-plugin-swc";
 import { createMiddleware } from './server';
@@ -11,6 +13,9 @@ export function VitePluginNode(cfg: VitePluginNodeConfig): Plugin[] {
     exportName: cfg.exportName ?? 'viteNodeApp'
   };
 
+  const singlelineCommentsRE = /\/\/.*/g;
+  const multilineCommentsRE = /\/\*(.|[\r\n])*?\*\//gm;
+
   const plugins: Plugin[] = [
     {
       name: PLUGIN_NAME,
@@ -21,6 +26,24 @@ export function VitePluginNode(cfg: VitePluginNodeConfig): Plugin[] {
         esbuild: config.tsCompiler === 'esbuild' ? {} : false,
         VitePluginNodeConfig: config,
       }),
+      transform(code, id) {
+        // only transform on development env
+        if (process.env.NODE_ENV === 'production') return;
+
+        // transform `import.meta.url` to `normal file url` as nodejs does
+        if (/import\.meta\.url/.test(code)) {
+          const fileUrl = pathToFileURL(path.resolve(process.cwd(), id)).href;
+          const source = 'import.meta.url';
+          const target = `'${fileUrl}'`;
+
+          return {
+            // first replace all, then retract all replacement for the comments
+            code: code.replaceAll(source, target)
+              .replace(singlelineCommentsRE, (v) => v.replaceAll(target, source))
+              .replace(multilineCommentsRE, (v) => v.replaceAll(target, source)),
+          }
+        }
+      },
       configureServer: (server) => {
         server.middlewares.use(createMiddleware(server))
       },
