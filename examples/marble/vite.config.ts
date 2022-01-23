635d4efd70ae7f546bc64f46ea56689a6d734959
Enrico Secondulfo <enricosecondulfo@Enricos-MBP.home-life.hub>
2022-01-23 23:02:06 +0100

    bump version for vite-plugin-node

--- examples/marble/vite.config.ts
+++ examples/marble/vite.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import { VitePluginNode } from './../../dist/index';
+import { VitePluginNode } from 'vite-plugin-node';
 
 export default defineConfig({
   build: {
