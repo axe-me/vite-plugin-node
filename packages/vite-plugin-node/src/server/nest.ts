65ed0d4cb906b92bb1c3ac7dce8c7a4db17e2503
Axe <axe@axe.dev>
2023-01-11 19:42:32 -0700

    code formating

--- packages/vite-plugin-node/src/server/nest.ts
+++ packages/vite-plugin-node/src/server/nest.ts
@@ -4,18 +4,20 @@ import type { RequestAdapter } from '..';
 let prevApp: INestApplication;
 
 export const NestHandler: RequestAdapter<INestApplication> = async ({ app, req, res }) => {
-  if (!(<any>app).isInitialized) {
+  // @ts-expect-error nest app typing error
+  if (!app.isInitialized) {
     if (prevApp)
       await prevApp.close();
 
     await app.init();
     prevApp = app;
   }
+
   const instance = app.getHttpAdapter().getInstance();
 
-  if(typeof instance === 'function')
+  if (typeof instance === 'function') {
     instance(req, res);
-  else {
+  } else {
     const fastifyApp = await instance.ready();
     fastifyApp.routing(req, res);
   }
